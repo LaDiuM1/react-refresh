030226ade79e00d33cd9394c3097c1455a37113a
LaDiuM <wjddhrtjd123@gmail.com>
2024-12-26 15:26:25 +0900

    일기 생성, 수정, 삭제 함수 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,46 +1,116 @@
 import './App.css';
-import {Routes, Route, Link, useNavigate} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Diary from "./pages/Diary.jsx";
 import New from "./pages/New.jsx";
 import Home from "./pages/Home.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
+import Edit from "./pages/Edit.jsx";
+import {createContext, useReducer, useRef} from "react";
 
-import getEmotionImage from "./util/get-emotion-image.js";
-
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
-function App() {
-    const nav = useNavigate();
-
-    const onClickButton = () => {
-      nav('/new');
+const mockData = [
+    {
+        id: 1,
+        createdDate: new Date().getTime(),
+        emotionId: 1,
+        content: '1번 일기 내용'
+    },
+    {
+        id: 2,
+        createdDate: new Date().getTime(),
+        emotionId: 2,
+        content: '2번 일기 내용'
     }
+]
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CREATE':
+            return [action.data, ...state];
+        case 'UPDATE': {
+            return state.map(item => (String(item.id) === String(action.data.id) ? action.data : item));
+        }
+        case 'DELETE':
+            return state.filter(item => String(item.id) !== String(action.data.id));
+    }
+}
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
+
+function App() {
+    const [data, dispatch] = useReducer(reducer, mockData);
+    const idRef = useRef(3);
+
+    // 새로운 일기 추가
+    const onCreate = (createdDate, emotionId, content) => {
+        dispatch({
+            id: idRef.current++,
+            type: 'CREATE',
+            data: {
+                createdDate,
+                emotionId,
+                content
+            }
+        });
+
+    }
+
+    // 기존 일기 수정
+    const onUpdate = (id, createdDate, emotionId, content) => {
+        dispatch({
+            type: 'UPDATE',
+            data: {
+                id,
+                createdDate,
+                emotionId,
+                content
+            }
+        });
+
+    }
+
+    // 기존 일기 삭제
+    const onDelete = (id) => {
+        dispatch({
+            type: 'DELETE',
+            data: {
+                id
+            }
+        })
+    }
+
 
     return (
         <>
-            <div>
-                <img src={getEmotionImage(1)}/>
-                <img src={getEmotionImage(2)}/>
-                <img src={getEmotionImage(3)}/>
-                <img src={getEmotionImage(4)}/>
-                <img src={getEmotionImage(5)}/>
-            </div>
+            <Header title={'Header'}
+                    leftChild={<Button text={'Left'}/>}
+                    rightChild={<Button text={'Right'}/>}
+            />
+            <button onClick={() => onCreate(new Date().getTime(), 2, '테스트')}>생성 테스트</button>
+            <button onClick={() => onUpdate(1, new Date().getTime(), 3, '1번 수정')}>수정 테스트</button>
+            <button onClick={() => onDelete(1)}>삭제 테스트</button>
 
-            <div>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/new'}>New</Link>
-                <Link to={'/diary'}>Diary</Link>
-            </div>
-            <button onClick={onClickButton}>New 페이지로 이동</button>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/new' element={<New/>}/>
-                <Route path='/diary/:id' element={<Diary/>}/>
-                <Route path='*' element={<NotFound/>}/>
-            </Routes>
+            <DiaryStateContext.Provider value={data}>
+                <DiaryDispatchContext.Provider
+                    value={{
+                        onCreate,
+                        onUpdate,
+                        onDelete
+                    }}
+                >
+                    <Routes>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/new' element={<New/>}/>
+                        <Route path='/diary/:id' element={<Diary/>}/>
+                        <Route path='/edit/:id' element={<Edit/>}/>
+                        <Route path='*' element={<NotFound/>}/>
+                    </Routes>
+                </DiaryDispatchContext.Provider>
+            </DiaryStateContext.Provider>
         </>
-  )
+    )
 }
 
 export default App
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -3,9 +3,8 @@ import {useSearchParams} from "react-router-dom";
 // 쿼리스트링 방식 파라미터 관리
 const Home = () => {
   const [params, setParams] = useSearchParams();
-  console.log(params.get('value'));
 
-  return <div>home</div>
+  return <div>Home</div>
 }
 
 export default Home;
