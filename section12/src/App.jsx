c68957f3e1af31ebd97545122940124c4441ac17
LaDiuM <wjddhrtjd123@gmail.com>
2025-01-06 16:56:18 +0900

    웹 스토리지 적용

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,60 +5,80 @@ import New from "./pages/New.jsx";
 import Home from "./pages/Home.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Edit from "./pages/Edit.jsx";
-import {createContext, useReducer, useRef} from "react";
-
-const mockData = [
-    {
-        id: 1,
-        createdDate: new Date('2024-12-01').getTime(),
-        emotionId: 1,
-        content: '1번 일기 내용'
-    },
-    {
-        id: 2,
-        createdDate: new Date('2024-11-05').getTime(),
-        emotionId: 2,
-        content: '2번 일기 내용'
-    },
-    {
-        id: 3,
-        createdDate: new Date('2025-01-06').getTime(),
-        emotionId: 4,
-        content: '4번 일기 내용'
-    },
-    {
-        id: 4,
-        createdDate: new Date('2024-12-30').getTime(),
-        emotionId: 5,
-        content: '5번 일기 내용'
-    }
-]
+import {createContext, useEffect, useReducer, useRef, useState} from "react";
 
 function reducer(state, action) {
+    let nextState;
+
     switch (action.type) {
-        case 'CREATE':
-            return [action.data, ...state];
-        case 'UPDATE': {
-            return state.map(item => (String(item.id) === String(action.data.id) ? action.data : item));
+        case 'INIT' :
+            return action.data;
+        case 'CREATE': {
+            nextState = [action.data, ...state];
+            break;
         }
-        case 'DELETE':
-            return state.filter(item => String(item.id) !== String(action.data.id));
+        case 'UPDATE': {
+            nextState = state.map(item => (String(item.id) === String(action.data.id)
+                ? action.data : item));
+            break;
+        }
+        case 'DELETE': {
+            nextState = state.filter(item => String(item.id) !== String(action.id));
+            break;
+        }
+        default:
+            return state;
     }
+
+    localStorage.setItem("diary", JSON.stringify(nextState));
+    return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function App() {
-    const [data, dispatch] = useReducer(reducer, mockData);
-    const idRef = useRef(5);
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, dispatch] = useReducer(reducer, []);
+    const idRef = useRef(0);
+
+    useEffect(() => {
+        const storedData = localStorage.getItem('diary');
+        console.log(storedData);
+        if(!storedData) {
+            setIsLoading(false);
+            return;
+        }
+        const parsedData = JSON.parse(storedData);
+        if(!Array.isArray(parsedData)) {
+            setIsLoading(false);
+            return;
+        }
+
+        let maxId = 0;
+        parsedData.forEach(item => {
+            if(Number(item.id) > maxId) {
+                maxId = item.id;
+            }
+        })
+
+        idRef.current = maxId + 1;
+
+        dispatch({
+            type: 'INIT',
+            data : parsedData
+        });
+        setIsLoading(false);
+    }, []);
+
+
 
     // 새로운 일기 추가
     const onCreate = (createdDate, emotionId, content) => {
         dispatch({
-            id: idRef.current++,
             type: 'CREATE',
             data: {
+                id: idRef.current++,
                 createdDate,
                 emotionId,
                 content
@@ -85,12 +105,14 @@ function App() {
     const onDelete = (id) => {
         dispatch({
             type: 'DELETE',
-            data: {
-                id
-            }
+            id
         })
     }
 
+
+    if(isLoading) {
+        return <div>데이터 로딩중입니다 ...</div>;
+    }
 
     return (
         <>
